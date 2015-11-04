2755f8094383a0906756634f7d2541023b9b65d1
Raymond Cheng <me@raymondcheng.net>
2015-11-04 14:57:17 -0600

    trying an integration test

--- test/demo.js
+++ test/demo.js
@@ -1,17 +1,121 @@
-var XhrXhr = require('freedom-xhr').corexhr;
-var XhrTcpsocket = require('freedom-xhr').coretcpsocket;
+//var XhrXhr = require('freedom-xhr').corexhr;
+//var XhrTcpsocket = require('freedom-xhr').coretcpsocket;
 
-var xhrdemo = function(dispatchEvent) {
+var xhrdemo = function(dispatchEvent, selector) {
   this.dispatch = dispatchEvent;
+  this.XhrClass = require('freedom-xhr')[selector];
+  this.xhr = new this.XhrClass();
 };
 
 xhrdemo.prototype.testHasXhr = function() {
-  if (typeof XhrXhr !== "undefined" &&
-        typeof XhrTcpsocket !== "undefined") {
-    return Promise.resolve();
+  if (typeof this.XhrClass !== "undefined" &&
+        typeof this.xhr !== "undefined") {
+    return Promise.resolve("Woo");
   } else {
-    return Promise.reject();
+    return Promise.reject("Missing XHR classes");
   }
 };
+
+xhrdemo.prototype.testLoadEvent = function() {
+  return new Promise(function(resolve, reject) {
+    console.log("testLoadEvent");
+    /**
+    this.xhr.addEventListener('load', function (e) {
+      // TODO: expect(e).not.toBeUndefined();
+      if (this.xhr.readyState === 4) {
+        resolve("Woo");
+      } else {
+        reject("readyState is not 4: " + this.xhr.readyState);
+      }
+    });
+    **/
+
+    console.log("1");
+    this.xhr.open('GET', 'http://isup.me');
+    console.log("2");
+    this.xhr.send(null);
+    console.log("!!!");
+  });
+};
+
+    /**
+    afterEach(function() {
+        xhr.abort();
+    });
+
+    it('error event', function(done) {
+        xhr.addEventListener('error', function (e) {
+            // TODO: expect(e).not.toBeUndefined();
+            done();
+        });
+
+        xhr.open('GET', 'http://no.such.domain');
+        xhr.send(null);
+    });
+
+    it('timeout event', function(done) {
+        xhr.addEventListener('timeout', function (e) {
+            // TODO: expect(e).not.toBeUndefined();
+            done();
+        });
+
+        xhr.timeout = 500;  // milliseconds
+        xhr.open('GET', 'http://192.0.2.1');  // Reserved IP address.
+        xhr.send(null);
+    });
+
+    it('beforeredirect event', function(done) {
+        var xhr = new chrome.sockets.tcp.xhr();
+
+        var redirector = 'http://httpredir.debian.org/debian/';
+        xhr.addEventListener('beforeredirect', function (redirectUrl, responseHeaders, statusText) {
+            expect(redirectUrl).toMatch(/^http.*\/debian\/$/);
+            expect(redirectUrl).not.toEqual(redirector);
+            expect(statusText).toMatch(/^30/);
+            done();
+        });
+
+        xhr.open('GET', redirector);
+        xhr.send(null);
+    });
+
+    it('load event should indicate a successful response', function (done) {
+        xhr.addEventListener('load', function (e) {
+            // TODO: expect(e).not.toBeUndefined();
+            expect(xhr.statusText).toBe('200 OK');
+            expect(xhr.responseText).toMatch(/&#9731;/);
+            done();
+        });
+
+        xhr.open('GET', 'http://unicodesnowmanforyou.com/');
+        xhr.send(null);
+    });
+
+    it ('done state should indicate a successful response', function (done) {
+        xhr.addEventListener('readystatechange', function () {
+            if (this.readyState === this.DONE) {
+                expect(xhr.statusText).toBe('200 OK');
+                expect(xhr.responseText).toMatch(/&#9731;/);
+                done();
+            }
+        });
+
+        xhr.open('GET', 'http://unicodesnowmanforyou.com/');
+        xhr.send(null);
+    });
+
+    it ('SSL support', function (done) {
+        xhr.addEventListener('readystatechange', function () {
+            if (this.readyState === this.DONE) {
+                expect(xhr.statusText).toBe('200 OK');
+                done();
+            }
+        });
+
+        xhr.open('GET', 'https://www.httpsnow.org/');
+        xhr.send(null);
+    });
+    **/
+
 
 freedom().providePromises(xhrdemo);
--- test/integration.js
+++ test/integration.js
@@ -1,116 +1,35 @@
 describe('Basic XMLHttpRequest integration tests', function() {
   var xhr;
 
-  var errHandler = function(done, errMsg) {
-    expect(errMsg).toBeUndefined();
-    expect(true).toBe(false);
-    done();
+  var runTest = function(done, selector, testName) {
+    freedom('scripts/test/demo.json').then(function(Demo) {
+      var demo = new Demo(selector);
+      demo[testName]().then(function(ret) {
+        expect(ret).toEqual(jasmine.any(String));
+        done();
+      }).catch(function(errMsg) {
+        console.log(errMsg);
+        expect(errMsg).toBeUndefined();
+        expect(true).toBe(false);
+        done();
+      });
+    });
   };
 
-  it('succeeds', function(done) {
-    freedom('scripts/test/demo.json').then(function(Demo) {
-      var demo = new Demo();
-      demo.testHasXhr().then(function() {
-        expect(true).toBe(true);
-        done();
-      }).catch(errHandler.bind({}, done));
-    });
+  it('corexhr: constructs XHRs', function(done) {
+    runTest(done, "corexhr", "testHasXhr");
   });
-    /**
-    beforeEach(function() {
-        xhr = new require('chrome.sockets.tcp.xhr')();
-    });
 
-    afterEach(function() {
-        xhr.abort();
-    });
+  it('coretcpsocket: constructs XHRs', function(done) {
+    runTest(done, "coretcpsocket", "testHasXhr");
+  });
 
-    it('should initiate an instance of chrome.sockets.tcp.xhr', function() {
-        expect(xhr instanceof chrome.sockets.tcp.xhr).toBe(true);
-    });
+  it('corexhr: load event', function(done) {
+    runTest(done, "corexhr", "testLoadEvent");
+  });
 
-    it('load event', function(done) {
-        xhr.addEventListener('load', function (e) {
-            // TODO: expect(e).not.toBeUndefined();
-            expect(xhr.readyState).toBe(xhr.DONE);
-            done();
-        });
+  it('coretcpsocket: load event', function(done) {
+    runTest(done, "coretcpsocket", "testLoadEvent");
+  });
 
-        xhr.open('GET', 'http://isup.me');
-        xhr.send(null);
-    });
-
-    it('error event', function(done) {
-        xhr.addEventListener('error', function (e) {
-            // TODO: expect(e).not.toBeUndefined();
-            done();
-        });
-
-        xhr.open('GET', 'http://no.such.domain');
-        xhr.send(null);
-    });
-
-    it('timeout event', function(done) {
-        xhr.addEventListener('timeout', function (e) {
-            // TODO: expect(e).not.toBeUndefined();
-            done();
-        });
-
-        xhr.timeout = 500;  // milliseconds
-        xhr.open('GET', 'http://192.0.2.1');  // Reserved IP address.
-        xhr.send(null);
-    });
-
-    it('beforeredirect event', function(done) {
-        var xhr = new chrome.sockets.tcp.xhr();
-
-        var redirector = 'http://httpredir.debian.org/debian/';
-        xhr.addEventListener('beforeredirect', function (redirectUrl, responseHeaders, statusText) {
-            expect(redirectUrl).toMatch(/^http.*\/debian\/$/);
-            expect(redirectUrl).not.toEqual(redirector);
-            expect(statusText).toMatch(/^30/);
-            done();
-        });
-
-        xhr.open('GET', redirector);
-        xhr.send(null);
-    });
-
-    it('load event should indicate a successful response', function (done) {
-        xhr.addEventListener('load', function (e) {
-            // TODO: expect(e).not.toBeUndefined();
-            expect(xhr.statusText).toBe('200 OK');
-            expect(xhr.responseText).toMatch(/&#9731;/);
-            done();
-        });
-
-        xhr.open('GET', 'http://unicodesnowmanforyou.com/');
-        xhr.send(null);
-    });
-
-    it ('done state should indicate a successful response', function (done) {
-        xhr.addEventListener('readystatechange', function () {
-            if (this.readyState === this.DONE) {
-                expect(xhr.statusText).toBe('200 OK');
-                expect(xhr.responseText).toMatch(/&#9731;/);
-                done();
-            }
-        });
-
-        xhr.open('GET', 'http://unicodesnowmanforyou.com/');
-        xhr.send(null);
-    });
-
-    it ('SSL support', function (done) {
-        xhr.addEventListener('readystatechange', function () {
-            if (this.readyState === this.DONE) {
-                expect(xhr.statusText).toBe('200 OK');
-                done();
-            }
-        });
-
-        xhr.open('GET', 'https://www.httpsnow.org/');
-        xhr.send(null);
-    });
-    **/
 });
