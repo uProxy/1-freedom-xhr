cb9c607515d08b684221334cd54cee2bc85ac17a
Ben Schwartz <bemasc@uproxy.org>
2016-03-17 18:41:54 -0400

    Add backend autoselection and typescript interface

---
+++ src/index.d.ts
@@ -0,0 +1,3 @@
+export var coretcpsocket : typeof XMLHttpRequest;
+export var corexhr : typeof XMLHttpRequest;
+export var auto : typeof XMLHttpRequest;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-
 module.exports.corexhr = require('./xhr.corexhr');
 module.exports.coretcpsocket = require('./xhr.coretcpsocket');
+// TODO: Move auto to coretcpsocket on Firefox 46+, once
+// freedom-for-firefox-0.6.23 is available (and it's tested).
+module.exports.auto = navigator.userAgent.indexOf('Firefox') >= 0 ?
+    module.exports.corexhr : module.exports.coretcpsocket;
